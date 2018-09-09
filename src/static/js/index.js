636c49482e6cbb9458b7d59545b34faad71aa4a0
i333656 <vasil.stoyanov@feg.bg>
2018-09-09 12:33:18 +0200

    display table and identification in front-end

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,4 +1,5 @@
-const graph_request = new XMLHttpRequest();
+const truth_table_request = new XMLHttpRequest();
+const rendered_truth_table = new XMLHttpRequest();
 
 document.getElementById("generate").addEventListener("click", function () {
     var input = document.getElementById("expression").value;
@@ -6,22 +7,33 @@ document.getElementById("generate").addEventListener("click", function () {
         alert("Please fill in a logical expression before generating a tree");
     } else {
         json = JSON.stringify({"expression": input});
-        graph_request.open("POST", "/expression_image", true);
-        graph_request.setRequestHeader("Content-Type", "application/json");
-        graph_request.send(json);
+        truth_table_request.open("POST", "/create_truth_table", true);
+        truth_table_request.setRequestHeader("Content-Type", "application/json");
+        truth_table_request.send(json);
     }
 });
 
-graph_request.onreadystatechange = function() {
+truth_table_request.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200){
         var json = JSON.parse(this.responseText);
-        var img_name = json['image'];
-        if(img_name) {
-            var img_container = document.getElementById("graph-image");
-            img_container.style.display = "block";
-            img_container.src = "../static/images/" + img_name;
-        } else {
+        var error = json['error'];
+        if(error) {
             alert(json["error"]);
+        } else {
+            var img_container = document.getElementById("graph-image");
+            img_container.src = "../static/images/" + json["image"];
+            rendered_truth_table.open("POST", "/render_table", true);
+            rendered_truth_table.setRequestHeader("Content-Type", "application/json");
+            rendered_truth_table.send(JSON.stringify({"table": json["table"], "identification": json["identification"]}));
         }
+    }
+};
+
+rendered_truth_table.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+        var img_container = document.getElementById("graph-image");
+        var table_container = document.getElementById("truth-table");
+        img_container.style.display = "block";
+        table_container.innerHTML = this.responseText;
     }
 };
--- src/views.py
+++ src/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, Response, json
 from src import app
-from src.models.Tree import *
+from src.models.TruthTable import *
 from src.models.Errors import ParserError
 
 
@@ -8,12 +8,20 @@ from src.models.Errors import ParserError
 def index():
     return render_template('index.html')
 
-@app.route('/expression_image', methods=['POST'])
-def get_expression_image():
+@app.route('/create_truth_table', methods=['POST'])
+def create_truth_table():
     if request.method == 'POST':
         data = request.json
         try:
-            t = Tree(data['expression'])
-            return Response(json.dumps({'image': t.get_graph_image_name()}), 200)
+            t = TruthTable(data['expression'])
+            return Response(json.dumps({'image': t.tree.get_graph_image_name(),
+                                        'table': t.table.tolist(),
+                                        'identification': t.identification}), 200)
         except ParserError as e:
             return Response(json.dumps({'error': e.message}), 200)
+
+@app.route('/render_table', methods=['POST'])
+def render_table():
+    if request.method == 'POST':
+        data = request.json
+        return render_template('table.html', table=data['table'], identification=data['identification'])
